ad5bb82d447d33e4067f2a2d30e4be0f91ddcec9
Tiago de Oliveira <olvrti@gmail.com>
2022-03-05 19:30:06 -0300

    fix: fix reset route

--- src/routes/reset-routes.ts
+++ src/routes/reset-routes.ts
@@ -4,6 +4,6 @@ import { deleteDataController } from '../controllers/delete-data'
 const resetRoutes = Router()
 
 resetRoutes
-  .post('/', (request, response) => deleteDataController.handle(request, response))
+  .post('/reset', (request, response) => deleteDataController.handle(request, response))
 
 export { resetRoutes }
