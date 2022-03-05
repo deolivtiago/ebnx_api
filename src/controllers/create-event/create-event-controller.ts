ec7f802c48ed608c91652eff174d3981c24c519b
Tiago de Oliveira <olvrti@gmail.com>
2022-03-05 19:35:42 -0300

    feat: update controller to handle event

--- src/controllers/create-event/create-event-controller.ts
+++ src/controllers/create-event/create-event-controller.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express'
 import { CreateDepositUsecase } from '../../usecases/create-deposit-usecase'
+import { CreateWithdrawUsecase } from '../../usecases/create-withdraw-usecase'
 
 export class CreateEventController {
-  constructor (private readonly _createDepositUsecase: CreateDepositUsecase) { }
+  constructor (
+    private readonly _createDepositUsecase: CreateDepositUsecase,
+    private readonly _createWithdrawUsecase: CreateWithdrawUsecase
+  ) { }
 
   handle (request: Request, response: Response): Response {
-    const { type, amount, destination } = request.body
+    const { type, amount, origin, destination } = request.body
 
     if (type === 'deposit') {
       const json = this._createDepositUsecase.execute({ amount, destination })
 
       return response.status(201).json(json)
+    }
+
+    if (type === 'withdraw') {
+      const json = this._createWithdrawUsecase.execute({ amount, origin })
+
+      return json === -1 ? response.status(404).json(0) : response.status(201).json(json)
     }
   }
 }
--- src/controllers/create-event/index.ts
+++ src/controllers/create-event/index.ts
@@ -1,13 +1,15 @@
 import { InMemoryAccountRepository } from '../../repositories/in-memory-account-repository'
 import { InMemoryEventRepository } from '../../repositories/in-memory-event-repository'
 import { CreateDepositUsecase } from '../../usecases/create-deposit-usecase'
+import { CreateWithdrawUsecase } from '../../usecases/create-withdraw-usecase'
 import { CreateEventController } from './create-event-controller'
 
 const accountRepository = InMemoryAccountRepository.getInstance()
 const eventRepository = InMemoryEventRepository.getInstance()
 
 const createDepositUsecase = new CreateDepositUsecase(eventRepository, accountRepository)
+const createWithdrawUsecase = new CreateWithdrawUsecase(eventRepository, accountRepository)
 
-const createEventController = new CreateEventController(createDepositUsecase)
+const createEventController = new CreateEventController(createDepositUsecase, createWithdrawUsecase)
 
 export { createEventController }
