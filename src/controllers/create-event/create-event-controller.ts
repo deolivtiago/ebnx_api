35e017aa602f731426a9b8527465abad700705dc
Tiago de Oliveira <olvrti@gmail.com>
2022-03-05 19:47:13 -0300

    feat: update controller to handle event

--- src/controllers/create-event/create-event-controller.ts
+++ src/controllers/create-event/create-event-controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express'
 import { CreateDepositUsecase } from '../../usecases/create-deposit-usecase'
+import { CreateTransferUsecase } from '../../usecases/create-transfer-usecase'
 import { CreateWithdrawUsecase } from '../../usecases/create-withdraw-usecase'
 
 export class CreateEventController {
   constructor (
     private readonly _createDepositUsecase: CreateDepositUsecase,
-    private readonly _createWithdrawUsecase: CreateWithdrawUsecase
+    private readonly _createWithdrawUsecase: CreateWithdrawUsecase,
+    private readonly _createTransferUsecase: CreateTransferUsecase
   ) { }
 
   handle (request: Request, response: Response): Response {
@@ -19,6 +21,12 @@ export class CreateEventController {
 
     if (type === 'withdraw') {
       const json = this._createWithdrawUsecase.execute({ amount, origin })
+
+      return json === -1 ? response.status(404).json(0) : response.status(201).json(json)
+    }
+
+    if (type === 'transfer') {
+      const json = this._createTransferUsecase.execute({ amount, origin, destination })
 
       return json === -1 ? response.status(404).json(0) : response.status(201).json(json)
     }
--- src/controllers/create-event/index.ts
+++ src/controllers/create-event/index.ts
@@ -1,6 +1,7 @@
 import { InMemoryAccountRepository } from '../../repositories/in-memory-account-repository'
 import { InMemoryEventRepository } from '../../repositories/in-memory-event-repository'
 import { CreateDepositUsecase } from '../../usecases/create-deposit-usecase'
+import { CreateTransferUsecase } from '../../usecases/create-transfer-usecase'
 import { CreateWithdrawUsecase } from '../../usecases/create-withdraw-usecase'
 import { CreateEventController } from './create-event-controller'
 
@@ -9,7 +10,12 @@ const eventRepository = InMemoryEventRepository.getInstance()
 
 const createDepositUsecase = new CreateDepositUsecase(eventRepository, accountRepository)
 const createWithdrawUsecase = new CreateWithdrawUsecase(eventRepository, accountRepository)
+const createTransferUsecase = new CreateTransferUsecase(eventRepository, accountRepository)
 
-const createEventController = new CreateEventController(createDepositUsecase, createWithdrawUsecase)
+const createEventController = new CreateEventController(
+  createDepositUsecase,
+  createWithdrawUsecase,
+  createTransferUsecase
+)
 
 export { createEventController }
