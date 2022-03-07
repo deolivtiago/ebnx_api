062cc4e9f4b71e68dc2c2f008a997f1bbe065119
Tiago de Oliveira <olvrti@gmail.com>
2022-03-07 00:08:02 -0300

    refactor: add exception handling

--- src/controllers/create-event/create-event-controller.ts
+++ src/controllers/create-event/create-event-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { NotFoundError } from '../../errors/not-found-error'
 import { CreateDepositUsecase } from '../../usecases/create-deposit-usecase'
 import { CreateTransferUsecase } from '../../usecases/create-transfer-usecase'
 import { CreateWithdrawUsecase } from '../../usecases/create-withdraw-usecase'
@@ -12,23 +13,26 @@ export class CreateEventController {
 
   handle (request: Request, response: Response): Response {
     const { type, amount, origin, destination } = request.body
+    let result = { status: 500, json: { } }
 
-    if (type === 'deposit') {
-      const json = this._createDepositUsecase.execute({ amount, destination })
-
-      return response.status(201).json(json)
+    try {
+      switch (type) {
+        case 'deposit':
+          result = { status: 201, json: this._createDepositUsecase.execute({ amount, destination }) }
+          break
+        case 'withdraw':
+          result = { status: 201, json: this._createWithdrawUsecase.execute({ amount, origin }) }
+          break
+        case 'transfer':
+          result = { status: 201, json: this._createTransferUsecase.execute({ amount, origin, destination }) }
+          break
+      }
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        result = { status: 404, json: 0 }
+      }
     }
 
-    if (type === 'withdraw') {
-      const json = this._createWithdrawUsecase.execute({ amount, origin })
-
-      return json === -1 ? response.status(404).json(0) : response.status(201).json(json)
-    }
-
-    if (type === 'transfer') {
-      const json = this._createTransferUsecase.execute({ amount, origin, destination })
-
-      return json === -1 ? response.status(404).json(0) : response.status(201).json(json)
-    }
+    return response.status(result.status).json(result.json)
   }
 }
--- src/controllers/get-balance/get-balance-controller.ts
+++ src/controllers/get-balance/get-balance-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { NotFoundError } from '../../errors/not-found-error'
 import { GetBalanceUsecase } from '../../usecases/get-balance-usecase'
 
 export class GetBalanceController {
@@ -6,9 +7,18 @@ export class GetBalanceController {
 
   handle (request: Request, response: Response): Response {
     const { account_id: id } = request.query
+    let result = { status: 500, json: { } }
 
-    const balance = this._getBalanceUsecase.execute(String(id))
+    try {
+      const balance = this._getBalanceUsecase.execute(String(id))
 
-    return balance === -1 ? response.status(404).json(0) : response.status(200).json(balance)
+      result = { status: 200, json: balance }
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        result = { status: 404, json: 0 }
+      }
+    }
+
+    return response.status(result.status).json(result.json)
   }
 }
